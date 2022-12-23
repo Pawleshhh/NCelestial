b6dba74c17a5628b037ad3a149b9a38be579df5a
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2022-12-23 15:59:01 +0100

    Create JulianDate with two main constructors

---
+++ Source/NCelestial.Core/Time/JulianDate.fs
@@ -0,0 +1,28 @@
+﻿namespace NCelestial.Core
+
+open NCelestial.Core.MathHelper
+
+type public JulianDate =
+    struct
+        val JulianDate: float
+
+        member this.Date =
+            0.0
+
+        new (julianDate) = 
+            // TODO: Check for max (System.DateTime)
+            if julianDate < 0.0 then
+                failwith "Julian date cannot be negative"
+
+            { JulianDate = julianDate }
+        
+        new (dayNumber, fractionOfDay) = 
+            if dayNumber |> abs |> fractionalPart <> 0.5 then 
+                failwith "Day number must have fractional part equal to 0.5"
+            if fractionOfDay |> notInRangeInclusive 0.0 1.0 then
+                failwith "Fraction of day must have fractional part only"
+
+            new JulianDate(dayNumber + fractionOfDay)
+
+
+    end
---
+++ Source/NCelestial.Core/Utilities/MathHelper.fs
@@ -0,0 +1,18 @@
+﻿module internal NCelestial.Core.MathHelper
+
+let fractionalPart (v: float) =
+     v - truncate v
+
+let inRangeExclusive min max value =
+    min < value && value < max
+
+let inRangeInclusive min max value =
+    min <= value && value <= max
+
+let notInRangeExclusive min max value =
+    inRangeExclusive min max value
+    |> not
+
+let notInRangeInclusive min max value =
+    inRangeInclusive min max value
+    |> not
